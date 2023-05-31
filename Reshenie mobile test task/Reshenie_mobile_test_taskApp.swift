3dc7199059e5e1e06016a29c0130223cf521b415
david <devid200590@gmail.com>
2023-05-31 19:22:02 +0300

    first commit

---
+++ Reshenie mobile test task/Reshenie_mobile_test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reshenie_mobile_test_taskApp.swift
+//  Reshenie mobile test task
+//
+//  Created by дэвид Кихтенко on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Reshenie_mobile_test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
